ff4d85405cbb0e8e3e7f624e8c4e31156a55c415
kaatenn <1924198467@qq.com>
2025-02-13 17:31:39 +0800

    修改指令存储方案

--- src/main/kotlin/moe/gensoukyo/easy_command/analyser/WordAnalyser.kt
+++ src/main/kotlin/moe/gensoukyo/easy_command/analyser/WordAnalyser.kt
@@ -1,5 +1,14 @@
 package moe.gensoukyo.easy_command.analyser
 
-class WordAnalyser {
+import moe.gensoukyo.easy_command.core.CommandManager
+import moe.gensoukyo.easy_command.core.GrammarTreeManager
+
+object WordAnalyser {
+    fun wordAnalyse() {
+        GrammarTreeManager.grammarForest.clear()
+        for (command in CommandManager.commands) {
+        }
+    }
+
 
 }
--- src/main/kotlin/moe/gensoukyo/easy_command/core/CommandManager.kt
+++ src/main/kotlin/moe/gensoukyo/easy_command/core/CommandManager.kt
@@ -3,11 +3,14 @@ package moe.gensoukyo.easy_command.core
 import com.google.gson.JsonObject
 
 object CommandManager {
-    private val commands = mutableMapOf<String, String>()
+    val commands = mutableListOf<String>()
 
     fun fromJson(json: JsonObject){
-        for ((key, value) in json.entrySet()) {
-            commands[key] = value.asString
+        val commandsArray = json.getAsJsonArray("commands")
+            ?: throw IllegalArgumentException("The json misses \"commands\" field")
+        commands.clear()
+        for (element in commandsArray) {
+            commands.add(element.asString)
         }
     }
 }
